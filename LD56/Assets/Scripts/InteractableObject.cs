223fdb2ebaca8b0a63072cec367dd407539a099a
NayYan0802 <103715225+NayYan0802@users.noreply.github.com>
2024-10-05 16:47:05 -0400

    interactable scripts update

--- LD56/Assets/Scripts/InteractableObject.cs
+++ LD56/Assets/Scripts/InteractableObject.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractableObject : MonoBehaviour
+public class InteractableObject : MonoBehaviour ,IInteractable
 {
-    // Start is called before the first frame update
-    void Start()
+    public int interactPriority = 0;
+
+    public int frightMeterInRange;
+    public int frightMeterOutRange;
+
+    public int GetInteractPriority()
     {
-        
+        return interactPriority;
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool Interact(InteractArgs args)
     {
-        
+        throw new System.NotImplementedException();
+    }
+
+    public void OnTargetedEnter()
+    {
+
+    }
+
+    public void OnTargetedExit()
+    {
+
     }
 }

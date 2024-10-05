3825351782110cb922f196e7022ab80159d91316
NayYan0802 <103715225+NayYan0802@users.noreply.github.com>
2024-10-05 18:30:30 -0400

    update pickable object

--- LD56/Assets/Scripts/PickableObject.cs
+++ LD56/Assets/Scripts/PickableObject.cs
@@ -10,6 +10,7 @@ public class PickableObject : MonoBehaviour
         this.transform.localPosition = Vector3.zero;
         this.GetComponent<SpriteRenderer>().sortingOrder = 6;
         this.GetComponent<Rigidbody2D>().gravityScale = 0;
+        this.gameObject.layer = 8;
         return this;
     }
 
@@ -18,5 +19,6 @@ public class PickableObject : MonoBehaviour
         this.transform.SetParent(null);
         this.GetComponent<SpriteRenderer>().sortingOrder = 0;
         this.GetComponent<Rigidbody2D>().gravityScale = Constant.gravityScale;
+        this.gameObject.layer = 7;
     }
 }

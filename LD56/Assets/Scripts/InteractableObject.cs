24084a8f28343432da37331c6ffa07527ada62bc
NayYan0802 <103715225+NayYan0802@users.noreply.github.com>
2024-10-05 17:09:40 -0400

    update interact

--- LD56/Assets/Scripts/InteractableObject.cs
+++ LD56/Assets/Scripts/InteractableObject.cs
@@ -1,3 +1,5 @@
+using Sirenix.OdinInspector;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +11,17 @@ public class InteractableObject : MonoBehaviour ,IInteractable
     public int frightMeterInRange;
     public int frightMeterOutRange;
 
+    [FoldoutGroup("InteractionType")]public bool push, pick, hide, turn, trigger;
+
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
     public int GetInteractPriority()
     {
         return interactPriority;
@@ -16,7 +29,22 @@ public class InteractableObject : MonoBehaviour ,IInteractable
 
     public bool Interact(InteractArgs args)
     {
-        throw new System.NotImplementedException();
+        if (push)
+        {
+            Push();
+        }
+
+        if (pick)
+        {
+
+        }
+        return false;
+    }
+
+    private void Push()
+    {
+        spriteRenderer.sortingOrder = -1;
+        rb.gravityScale = 1;
     }
 
     public void OnTargetedEnter()

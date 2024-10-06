9f925cef84440496c6650c1ea63cb67f4839ff78
Targy <47155941+Targy@users.noreply.github.com>
2024-10-05 22:13:35 -0400

    Game Manager linked with UI

---
+++ LD56/Assets/Scripts/GameManagement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Sirenix.OdinInspector;
+using TMPro;
+
+public class GameManagement : MonoBehaviour
+{
+	[SerializeField, BoxGroup("Time")] int RoundMinute;
+	[SerializeField, BoxGroup("Time")] int RoundSecond;
+
+	[SerializeField, BoxGroup("UI")] TMP_Text UITime;
+	[SerializeField, BoxGroup("UI")] Image[] PeopleIndicators;
+	[SerializeField, BoxGroup("UI")] Sprite NumberMeter_Full;
+	[SerializeField, ReadOnly] private int scareNum = 0;
+	public static GameManagement Instance { get; private set; }
+	private void Start()
+	{
+		StartCoroutine(CountDown());
+	}
+
+	public void ScareOnePeople()
+	{
+		scareNum++;
+		PeopleIndicators[scareNum - 1].sprite = NumberMeter_Full;
+		if (scareNum >= 3)
+		{ 
+			//game success
+		}
+	}
+
+	IEnumerator CountDown()
+	{
+		while (RoundMinute != 0 || RoundSecond != 0)
+		{ 
+			RoundSecond--;
+			if (RoundSecond < 0)
+			{
+				RoundMinute--;
+				RoundSecond = 59;
+			}
+			UITime.text = $"{RoundMinute} : {RoundSecond}";
+			yield return new WaitForSeconds(1f);
+		}
+		//game fail
+	}
+}

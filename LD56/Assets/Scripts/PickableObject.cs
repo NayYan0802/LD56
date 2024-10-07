7fbda70da12c8675ba0beba439bf37c6977a0c5a
Leo Ji <sj3026@nyu.edu>
2024-10-07 17:19:41 -0400

    more audio added

--- LD56/Assets/Scripts/PickableObject.cs
+++ LD56/Assets/Scripts/PickableObject.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MoreMountains.Feedbacks;
 
 public class PickableObject : MonoBehaviour
 {
+    [SerializeField] MMF_Player pickupFB, putdownFB;
     public PickableObject PickUp(Player picker)
     {
         this.transform.SetParent(picker.pickOffset);
@@ -12,6 +14,8 @@ public class PickableObject : MonoBehaviour
         this.GetComponent<Rigidbody2D>().gravityScale = 0; 
         this.GetComponent<Rigidbody2D>().isKinematic = true;
         this.gameObject.layer = 8;
+        if (pickupFB != null)
+            pickupFB.PlayFeedbacks();
         return this;
     }
 
@@ -23,5 +27,7 @@ public class PickableObject : MonoBehaviour
         this.GetComponent<Rigidbody2D>().gravityScale = Constant.gravityScale;
         this.GetComponent<Rigidbody2D>().isKinematic = false;
         this.gameObject.layer = 7;
+        if (putdownFB != null)
+            putdownFB.PlayFeedbacks();
     }
 }

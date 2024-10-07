a0bf8aa9e38bfb1880bd37a99cf20c1fbdc463b2
Targy <47155941+Targy@users.noreply.github.com>
2024-10-06 20:45:41 -0400

    player ladder fix

--- LD56/Assets/Scripts/Player/PlayerState_Ladder.cs
+++ LD56/Assets/Scripts/Player/PlayerState_Ladder.cs
@@ -8,6 +8,8 @@ public class PlayerState_Ladder : PlayerState_Base
 	[SerializeField, BoxGroup("Setting")] float m_ladderSpeed;
 	public override void EnterState(StateMachine stateMachineContext)
 	{
+		LayerMask mask = LayerMask.GetMask("Default");
+		m_playerInputSystems.GetComponent<EdgeCollider2D>().excludeLayers = mask;
 		m_playerInputSystems.moveSpeed = m_ladderSpeed;
 		m_playerInputSystems.allowMoveVertical = true;
 		m_playerInputSystems.ToggleGravity(false);
@@ -16,6 +18,9 @@ public class PlayerState_Ladder : PlayerState_Base
 
 	public override void ExitState(StateMachine stateMachineContext)
 	{
+		LayerMask mask = 0;
+		m_playerInputSystems.GetComponent<EdgeCollider2D>().excludeLayers = mask;
+		m_playerInputSystems.gameObject.layer = 9;
 		m_playerInputSystems.ToggleGravity(true);
 		m_playerInputSystems.allowMoveVertical = false;
 		base.ExitState(stateMachineContext);

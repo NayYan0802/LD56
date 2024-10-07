c2dcccf65a77a9f51813a27abc4cfac7a57d375e
Leo Ji <sj3026@nyu.edu>
2024-10-07 17:39:28 -0400

    hit ground sfx

--- LD56/Assets/ItemFall.cs
+++ LD56/Assets/ItemFall.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MoreMountains.Feedbacks;
 
 public class ItemFall : MonoBehaviour
 {
+    [SerializeField] MMF_Player hitGroundFB;
     public GameObject PuffAnimation;
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,6 +14,7 @@ public class ItemFall : MonoBehaviour
             Instantiate(PuffAnimation,collision.bounds.center,Quaternion.identity);
             collision.gameObject.GetComponent<InteractableObject>().Scared(collision.bounds.center.x);
             Destroy(collision.gameObject);
+            hitGroundFB.PlayFeedbacks();
         }
     }
 }
